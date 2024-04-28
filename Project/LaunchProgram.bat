ec18279425cf38ca2165e3b147099bc8e33aa0c7
spencerpopa <104792456+spencerpopa@users.noreply.github.com>
2024-04-28 15:23:08 -0500

    Delete Project directory
    Old Project deleted

--- Project/LaunchProgram.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting server...
-start cmd /k "cd /d %~dp0 & python server.py"
-timeout /T 5 /nobreak
-start http://127.0.0.1:5000/
--- Project/README.txt
+++
@@ -1,5 +0,0 @@
-pip install Flask
-pip install flask-socketio
-
-To Run Project:
-	Double Click on the Batch file: "LaunchProgram.bat"
